8e5444def11092ca12d3fbfe042a22b953c8fe71
Freitas <freitas.heusx@gmail.com>
2024-12-24 12:43:13 -0300

    challenge 3

---
+++ day18/day18.py
@@ -0,0 +1,19 @@
+# modules, package
+
+from turtle import Turtle,Screen
+
+tu = Turtle()
+
+## Challenge 3
+# num_size = 3
+# for i in range(5):
+#     for _ in range(num_size):
+#         angle = 360/num_size
+#         tu.forward(100)
+#         tu.right(angle)
+#     num_size += 1
+    
+    
+    
+screen = Screen()
+screen.exitonclick()
